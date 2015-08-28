542425d3e3a5a95a06959d4e228e70b66be8e642
mr678 <matthias.reining@outlook.com>
2015-08-28 17:28:55 +0200

    update

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $("#contact input,textarea").jqBootstrapValidation({
+    $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
